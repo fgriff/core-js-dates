d9ff1cd201ea6af5233f4c2bc2c1329f3285833a
fgriff <anglirum@gmail.com>
2025-03-22 01:52:05 +0300

    feat: solve getNextFriday task

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -65,8 +65,13 @@ function getDayName(date) {
  * Date('2024-02-13T00:00:00Z') => Date('2024-02-16T00:00:00Z')
  * Date('2024-02-16T00:00:00Z') => Date('2024-02-23T00:00:00Z')
  */
-function getNextFriday(/* date */) {
-  throw new Error('Not implemented');
+function getNextFriday(date) {
+  const d = new Date(date);
+  const dayOfWeek = d.getDay();
+
+  return new Date(
+    d.setHours(d.getHours() + ((7 - dayOfWeek + 5) % 7 || 7) * 24)
+  );
 }
 
 /**
